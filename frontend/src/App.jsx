afea3c6967096b184c21db72daa5e22fdf175801
AsmaaMusa <s12113970@stu.najah.edu>
2026-04-24 23:02:11 +0300

    F4 front

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar.jsx'
-import ProjectsList from './pages/ProjectsList'
-import CreateProject from './pages/CreateProject'
-import SectionSelection from './pages/SectionSelection'
-import EditProject from './pages/EditProject'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import ReportPage from './pages/ReportPage.jsx'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-[#F0F0E8]">
-        <Navbar />
-        <main className="max-w-5xl mx-auto px-6 py-10">
-          <Routes>
-            <Route path="/projects" element={<ProjectsList />} />
-            <Route path="/projects/new" element={<CreateProject />} />
-            <Route path="/projects/:id/select" element={<SectionSelection />} />
-            <Route path="/projects/:id/edit" element={<EditProject />} />
-          </Routes>
-        </main>
-      </div>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ReportPage />} />
+      <Route path="/report" element={<ReportPage />} />
+    </Routes>
   )
 }
 
